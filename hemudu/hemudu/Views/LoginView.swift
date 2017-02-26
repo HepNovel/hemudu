fcd3806190c991563761c8135e1a4ac93868937a
Ran Cao <Kohaku@kohakus-macbook-pro.local>
2017-02-26 20:43:56 +0800

    20170226 2043

--- hemudu/hemudu/Views/LoginView.swift
+++ hemudu/hemudu/Views/LoginView.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 
-class LoginView: UIViewController {
+var projectiveUsername:String? = nil
+var projectiveAge:Int = 1
+var projectiveSex:String? = nil
+var projectiveID:Int? = nil
+var projectiveLevel:String? = nil
+
+class LoginView: UIViewController, UITextViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
     
     override func didReceiveMemoryWarning() {
