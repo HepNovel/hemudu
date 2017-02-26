cc7d5ef7d7a89bbdc806f7e9c5de26d0af059a92
Ran Cao <Kohaku@kohakus-macbook-pro.local>
2017-02-26 19:18:00 +0800

    20170226 1917

---
+++ hemudu/hemudu/Views/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  hemudu
+//
+//  Created by Ran Cao on 2017/2/26.
+//  Copyright © 2017年 Ran Cao. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
