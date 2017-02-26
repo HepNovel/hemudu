c3e66297abd702f27815ed37a6c0761ccbe3046e
Ran Cao <Kohaku@kohakus-macbook-pro.local>
2017-02-26 17:44:15 +0800

    lol

---
+++ hemudu/hemudu/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  hemudu
+//
+//  Created by Ran Cao on 2017/2/26.
+//  Copyright © 2017年 Ran Cao. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
