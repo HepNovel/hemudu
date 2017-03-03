3b9390c38e67f2fb0717a76087b114f24835750c
Ran Cao <Kohaku@kohakus-macbook-pro.local>
2017-03-03 22:24:35 +0800

    l

--- hemudu/hemudu/Views/LoginView.swift
+++ hemudu/hemudu/Views/LoginView.swift
@@ -15,21 +15,54 @@ var projectiveAge:Int = 1
 var projectiveSex:String? = nil
 var projectiveID:Int? = nil
 var projectiveLevel:String? = nil
+var projectiveMoney:String? = nil
 
-class LoginView: UIViewController, UITextViewDelegate {
+class LoginView: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var password: UITextField!
-    @IBAction func loginDo(sender: AnyObject) {
-        let Username = username.text
-        let Password = password.text
+    @IBOutlet weak var LgoinFaild: UILabel!
+    @IBAction func LoginBtn(_ sender: Any) {
+        let Username:String = username.text!
+        let Password:String = password.text!
         let url = "https://hepnovel.com/hemudu/ios/login.php"
         let para = ["username":Username, "password":Password]
-        request(url, method: .post, parameters: para, encoding:JSONEncoding.default).responseJSON { (response) in
-            print(response.data!)
+        var LoginSuc:Bool = false
+        
+        request(url, method: .post, parameters: para).responseJSON { (response) in
+            let data = response.result.value
+            let json = JSON(data)
+            print(json)
+            let name = json["username"]
+            let pass = json["password"]
+            let age = json["age"]
+            let sex = json["sex"]
+            let ID = json["id"]
+            let level = json["level"]
+            let money = json["money"]
+            if name == nil && pass == nil {
+                LoginSuc = false
+                print(LoginSuc)
+                self.LgoinFaild.text = "登录失败"
+            }else {
+                LoginSuc = true
+                print(LoginSuc)
+                projectiveUsername = String(describing: name)
+                projectiveAge = Int(String(describing: age))!
+                projectiveSex = String(describing: sex)
+                projectiveID = Int(String(describing: ID))
+                projectiveLevel = String(describing: level)
+                projectiveMoney = String(describing: money)
+                self.performSegue(withIdentifier: "login", sender: self)
+            }
+        }
+        if LoginSuc == true {
+           
+        }else{
+            
         }
         
-        
     }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
