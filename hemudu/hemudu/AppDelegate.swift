304b94b4aa1e35b653d8c4e8db7997edbd168117
Ran Cao <Kohaku@kohakus-macbook-pro.local>
2017-03-15 22:04:52 +0800

    Push Up

--- hemudu/hemudu/AppDelegate.swift
+++ hemudu/hemudu/AppDelegate.swift
@@ -7,7 +7,10 @@
 //
 
 import UIKit
-import UserNotifications
+
+let AliPush_key = "23692862"
+let AliPush_Secret = "55b833459307b138bedc55de0ec99286"
+let AliPush_MessageNoti = "CCPDidReceiveMessageNotification"
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,10 +18,57 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?, performActionForShortcutItem shortcutItem: UIApplicationShortcutItem, completionHandler: (Bool) -> Void) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         //registerAppNotificationSettings(launchOptions: launchOptions as [NSObject : AnyObject]?)
+        
+        initCloudPush(application: application)
+        CloudPushSDK.handleLaunching(launchOptions)
+        
         return true
+    }
+    
+    func initCloudPush(application:UIApplication) {
+        CloudPushSDK.asyncInit(AliPush_key, appSecret: AliPush_Secret, callback: nil)
+        let settings = UIUserNotificationSettings(types: [.alert,.badge,.sound], categories: nil)
+        if #available(iOS 8.0, *){
+            application.registerUserNotificationSettings(settings)
+            application.registerForRemoteNotifications()
+        }
+        registerMessageReceive()
+    }
+    
+    @nonobjc func application(application:UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken:NSData) {
+        CloudPushSDK.registerDevice(deviceToken as Data!, withCallback: nil)
+        
+    }
+    
+    func application(application:UIApplication, didFailToRegisterForRemoteNotificationsWithError error:NSError) {
+        print("did fail to reg fo remote notif with error:\(error.localizedDescription)")
+    }
+    
+    func registerMessageReceive() {
+        NotificationCenter.default.addObserver(self, selector: #selector(AppDelegate.onMessageReceived(notification:)),name:NSNotification.Name(rawValue: AliPush_MessageNoti), object: nil)
+    }
+    
+    func onMessageReceived(notification: NSNotification) {
+        let message:CCPSysMessage = notification.object as! CCPSysMessage
+        let title:NSString = NSString(data: message.title, encoding: String.Encoding.utf8.rawValue)!
+        let body:NSString = NSString(data: message.body, encoding: String.Encoding.utf8.rawValue)!
+        print("Received Mes title:\(title), centent:\(body)")
+    }
+    
+    func application(application:UIApplication, didReceiveRemoteNotification userInfo:[NSObject:AnyObject]) {
+        print("Receive one notif")
+        /*
+        let aps_dic:NSDictionary = userInfo["aps"] as! NSDictionary
+        let content = aps_dic.value(forKey:"alert")
+        let badge = aps_dic.value(forKey: "badge")?.integervalue
+        let sound = aps_dic.value(forKey: "sounde")
+        let extras = aps_dic.value(forKey: "Extras")
+ */
+        application.applicationIconBadgeNumber = 0
+        CloudPushSDK.handleReceiveRemoteNotification(userInfo)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -37,114 +87,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        // 触发3D Touch的type
-        var activeFrom3Dtouch:ShortcutIdentifier?
-        // 3Dtouch的type
-        enum ShortcutIdentifier:String {
-            // 当面收款
-            case Reg = "Reg"
-        }
-        //
-        @available(iOS 9.0, *)
-        func application(application: UIApplication, performActionForShortcutItem shortcutItem: UIApplicationShortcutItem, completionHandler: (Bool) -> Void) {
-            activeFrom3Dtouch = ShortcutIdentifier(rawValue: shortcutItem.type)
-        }
-        
-        func applicationDidBecomeActive(application: UIApplication) {
-            // 判断入口
-            guard let touchType = activeFrom3Dtouch else {
-                return
-            }
-            
-            // 分类处理
-            if touchType == .Reg {
-                // 延时0.5就足够了
-                DispatchQueue.main.asyncAfter(deadline: .now() + 20) {
-                    // 获取活动的Controller
-                    guard let currentViewController = self.window?.visibleViewController else {
-                        return
-                    }
-     
-                    let viewController = RegView()
-                    
-                    // 判断是否模态
-                    if(currentViewController.navigationController == nil) {
-                        currentViewController.present(viewController, animated: true, completion: nil)
-                    } else {
-                        currentViewController.navigationController?.pushViewController(viewController, animated: true)
-                    }
-                }
-            }
-            
-            // TODO: 判断跳转操作
-            
-            // 清除，防止再次激活应用重复触发
-            activeFrom3Dtouch = nil
-        }
-        
-        
+    
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-/*
-    private func registerAppNotificationSettings(launchOptions: [NSObject: AnyObject]?) {
-        if #available(iOS 10.0, *) {
-            let notifiCenter = UNUserNotificationCenter.current()
-            notifiCenter.delegate = self
-            let types = UNAuthorizationOptions(arrayLiteral: [.alert, .badge, .sound])
-            notifiCenter.requestAuthorization(options: types) { (flag, error) in
-                if flag {
-                    MPrintLog("iOS request notification success")
-                }else{
-                    MPrintLog(" iOS 10 request notification fail")
-                }
-            }
-        }
-        
-        UIApplication.shared.registerForRemoteNotifications()
-    }
-    
-    @available(iOS 10.0, *)
-    
-    func userNotificationCenter(center: UNUserNotificationCenter, willPresentNotification notification: UNNotification, withCompletionHandler completionHandler: (UNNotificationPresentationOptions) -> Void){
-        let userInfo = notification.request.content.userInfo
-        print("userInfo10:\(userInfo)")
-        completionHandler([.sound,.alert])
-        
-    }
-    
-    //iOS10新增：处理后台点击通知的代理方法
-    @available(iOS 10.0, *)
-    func userNotificationCenter(center: UNUserNotificationCenter, didReceiveNotificationResponse response: UNNotificationResponse, withCompletionHandler completionHandler: () -> Void){
-        let userInfo = response.notification.request.content.userInfo
-        print("userInfo10:\(userInfo)")
-        completionHandler()
-    }
-*/
-    
-    
 }
-
-extension UIWindow {
-    // 获取当前活动的控制器
-    public var visibleViewController:UIViewController? {
-        return UIWindow.getVisibleViewControllerFrom(vc: self.rootViewController)
-    }
-    
-    public static func getVisibleViewControllerFrom(vc:UIViewController?) -> UIViewController? {
-        if let nc = vc as? UINavigationController {
-            return UIWindow.getVisibleViewControllerFrom(vc: nc.visibleViewController)
-        } else if let tc = vc as? UITabBarController {
-            return UIWindow.getVisibleViewControllerFrom(vc: tc.selectedViewController)
-        } else {
-            if let pvc = vc?.presentedViewController {
-                return UIWindow.getVisibleViewControllerFrom(vc: pvc)
-            } else {
-                return vc
-            }
-        }
-    }
-}
-
---
+++ hemudu/hemudu/bridge.h
@@ -0,0 +1,9 @@
+//
+//  bridge.h
+//  hemudu
+//
+//  Created by Ran Cao on 2017/3/12.
+//  Copyright © 2017年 Ran Cao. All rights reserved.
+//
+
+#import <CloudPushSDK/CloudPushSDK.h>
